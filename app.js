1a766e457de7e2a54e7a3f37410211409e7ff957
Trịnh Quốc Việt <v.viettq12@vinhomes.vn>
2022-08-13 09:19:12 +0700

    Update variable in html

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+var fs = require('fs'),
+    path = require('path'),
+    filePath = path.join(__dirname, 'documents/index.html');
+
 app.listen(port, () => {
-console.log('listening for request on port 8080');
+    console.log('listening for request on port 8080');
 });
 app.get('/', (req, res) => {
-console.log('request made');
-res.sendFile("./documents/index.html", { root: __dirname })
+    console.log('request made');
+    res.sendFile("./documents/index.html", { root: __dirname })
 });
 app.get('/about', (req, res) => {
-res.sendFile('./documents/about.html', { root: __dirname });
+    res.sendFile('./documents/about.html', { root: __dirname });
 });
 app.get('/about-us', (req, res) => {
-res.redirect('/about');
+    //res.redirect('/about');
+    fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+        if (!err) {
+            //console.log('received data: ' + data);
+            console.log('MY_POD_NAMESPACE: ' + process.env.MY_POD_NAMESPACE);
+            console.log('MY_POD_NAME: ' + process.env.MY_POD_NAME);
+            console.log('MY_POD_IP: ' + process.env.MY_POD_IP);
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.write(data);
+            response.end();
+        } else {
+            console.log(err);
+        }
+    });
 });
 app.use((req, res) => {
-res.status(404).sendFile('./documents/404.html', { root: __dirname });
+    res.status(404).sendFile('./documents/404.html', { root: __dirname });
 });
